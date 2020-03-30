1023cb27db1903bb458b54c9abf5610ed6fe7cd5
kent0071 <kent0071@i.ua>
2020-03-30 17:38:28 +0300

    clean index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 let quoteArray = 
-    [{textQuote: "Quote1", citeQuote:"&copy; Autor1"},
+    [
+    {textQuote: "Quote1", citeQuote:"&copy; Autor1"},
     {textQuote: "Quote2", citeQuote:"&copy; Autor2"},
     {textQuote: "Quote3", citeQuote:"&copy; Autor3"},
     {textQuote: "Quote4", citeQuote:"&copy; Autor4"},
@@ -10,26 +11,7 @@ let quoteArray =
     {textQuote: "Quote7", citeQuote:"&copy; Autor7"},
     {textQuote: "Quote8", citeQuote:"&copy; Autor8"},
     {textQuote: "Quote9", citeQuote:"&copy; Autor9"},
-    {textQuote: "Quote10", citeQuote:"&copy; Autor10"}];
+    {textQuote: "Quote10", citeQuote:"&copy; Autor10"}
+];
 
-    function generatorQuote(array) {
-        let button = document.body.querySelector('button');
-        
-        button.addEventListener('click', function(){
-            let quoteText = document.body.querySelector('.quote__blockquote p');
-            let quoteCite = document.body.querySelector('.quote__blockquote cite');
-
-            let currentQuote = array[Math.floor(Math.random()*10)];
-                
-            quoteText.innerHTML = currentQuote.textQuote;
-            quoteCite.innerHTML = currentQuote.citeQuote;
-
-        });
-        button.addEventListener('click', function(){
-            button.classList.toggle('rotate');
-            setTimeout(() => button.classList.remove("rotate"), 300);
-            
-        });
-    }
-
-    generatorQuote(quoteArray);
+    
