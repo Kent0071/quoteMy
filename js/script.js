e68decd0c7554bae6787243d4b863393ff6c5e1d
kent0071 <kent0071@i.ua>
2020-03-31 12:13:38 +0300

    добавил модуль и jsdoc

--- js/script.js
+++ js/script.js
@@ -1,52 +1,89 @@
 'use strict';
 
-let quoteArray = 
-    [
-    {textQuote: "Quote1", citeQuote:"&copy; Autor1"},
-    {textQuote: "Quote2", citeQuote:"&copy; Autor2"},
-    {textQuote: "Quote3", citeQuote:"&copy; Autor3"},
-    {textQuote: "Quote4", citeQuote:"&copy; Autor4"},
-    {textQuote: "Quote5", citeQuote:"&copy; Autor5"},
-    {textQuote: "Quote6", citeQuote:"&copy; Autor6"},
-    {textQuote: "Quote7", citeQuote:"&copy; Autor7"},
-    {textQuote: "Quote8", citeQuote:"&copy; Autor8"},
-    {textQuote: "Quote9", citeQuote:"&copy; Autor9"},
-    {textQuote: "Quote10", citeQuote:"&copy; Autor10"}
-];
+(function () {
 
-function getElement(selector) {
+    let quoteArray =
+        [
+            { textQuote: "Quote1", citeQuote: "&copy; Autor1" },
+            { textQuote: "Quote2", citeQuote: "&copy; Autor2" },
+            { textQuote: "Quote3", citeQuote: "&copy; Autor3" },
+            { textQuote: "Quote4", citeQuote: "&copy; Autor4" },
+            { textQuote: "Quote5", citeQuote: "&copy; Autor5" },
+            { textQuote: "Quote6", citeQuote: "&copy; Autor6" },
+            { textQuote: "Quote7", citeQuote: "&copy; Autor7" },
+            { textQuote: "Quote8", citeQuote: "&copy; Autor8" },
+            { textQuote: "Quote9", citeQuote: "&copy; Autor9" },
+            { textQuote: "Quote10", citeQuote: "&copy; Autor10" }
+        ];
+
+    /**
+     * эта функция возвращает найденный элемент по селектору
+     * @returns {object|undefined}
+     * @param {string} строка с названием селектора
+     */
+
+    function getElementBySelector(selector) {
+
+        return document.body.querySelector(selector);
+
+    }
     
-    return document.body.querySelector(selector); 
+    /**
+     * эта функция возвращает рандомный элемент массива
+     * @returns {number|undefined}
+     * @param {Array} ссылка на обрабатываемый массив
+     */
 
-}
+    function getRandomQuote(array) {
 
-function currentQuote(array){
+        let currentQuote = array[Math.floor(Math.random() * 10)];
+        return currentQuote;
 
-    let currentQuote = array[Math.floor(Math.random()*10)];
-   return currentQuote;
+    }
 
-}
+    /**
+     * эта функция меняет содержимое html тега
+     *
+     * @param {Array} ссылка на обрабатываемый массив
+     */
+    function changeQuoteInnerHTML(arr) {
 
-let button = getElement('#button');
-let quoteText = getElement('.quote__blockquote p');
-let quoteCite = getElement('.quote__blockquote cite');
+        let activeQuote = getRandomQuote(arr);
 
-
-
-
-function generatorQuote(arr) {
-    
-    button.addEventListener('click', function(){
-        
-         let activeQuote = currentQuote(arr);
-            
         quoteText.innerHTML = activeQuote.textQuote;
         quoteCite.innerHTML = activeQuote.citeQuote;
-        
+    }
+
+    /**
+     * Функция которая меняет состояние кнопки
+     */
+    function changesBehaviorEventButton () {
+
         button.classList.toggle('rotate');
         setTimeout(() => button.classList.remove("rotate"), 300);
-    });
-}
+    }
 
-generatorQuote(quoteArray);
-    
+  
+    let button = getElementBySelector('#button');
+    let quoteText = getElementBySelector('.quote__blockquote p');
+    let quoteCite = getElementBySelector('.quote__blockquote cite');
+
+     /**
+     * эта функция меняет содержимое html тега при наступлении события
+     *
+     * @param {Array} ссылка на обрабатываемый массив
+     */
+
+    function initQuoteGenerator(arr) {
+
+        button.addEventListener('click', function () {
+            changeQuoteInnerHTML(arr);
+            
+            changesBehaviorEventButton();
+
+        });
+    }
+
+    initQuoteGenerator(quoteArray);
+})()
+
